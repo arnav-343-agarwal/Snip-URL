fc55d8b6a32223e0ba1599f764bffa1d406f5713
Arnav Agarwal <agarwalarnav2003@gmail.com>
2025-05-07 19:21:18 +0530

    Navbar Home link up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
-'use client';
+"use client";
 
+import Link from "next/link";
 import { Button } from "./ui/button";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 import { ExternalLink } from "lucide-react";
 
 export default function Navbar() {
   return (
     <nav className="backdrop-blur-sm bg-[#0e0e10]/90 text-white shadow-lg border-b border-white/10">
       <div className="max-w-screen-xl mx-auto px-4 py-3 flex justify-between items-center">
-        <h1 className="text-3xl font-bold tracking-wide text-white">QuickURL</h1>
-
+        <h1 className="text-3xl font-bold tracking-wide text-white">
+          <Link href="/" className="hover:underline">
+            QuickURL
+          </Link>
+        </h1>
         <div className="flex items-center space-x-4">
           <TooltipProvider>
             {[
